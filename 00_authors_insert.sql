3aed0596da230ee6075ce48009a8d3c317ac4329
Dmitry <latnikov@gmail.com>
2020-01-17 00:40:56 +0200

    additional authors

---
+++ 00_authors_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO authors (id, name, surname, birth_date) 
+VALUES (1, "Jack", "London", DATE("1876-01-12"));
+
+INSERT INTO authors (id, name, surname, birth_date) 
+VALUES  (2, "Ernest", "Hemingway", DATE("1899-07-21")), 
+        (3, "Mark", "Twain", DATE("1835-11-30")),
+        (4, "Oscar", "Wilde", DATE("1854-10-16")),
+        (5, "Charles", "Dickens", DATE("1812-02-07"));
